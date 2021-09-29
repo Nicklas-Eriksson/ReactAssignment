da36d0191ef124b2db9433202312757542352069
Nicklas Eriksson <salk1n@hotmail.com>
2021-09-29 15:07:07 +0200

    Fixade upg 4
    la till så man kan klicka på en färg. Den då tas man till en annan vy där man får visat vad man valde via uselocation

--- src/components/leftSidebar/LeftSidebar.js
+++ src/components/leftSidebar/LeftSidebar.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './LeftSidebar.css'
 import Avatar from '../../shared/img/icons/avatar.png'
 import User from '../user/GetUser'
+import {useHistory} from 'react-router-dom'
+import Route from '../../routes/RoutingPath'
 
 function LeftSidebar() {
+    const history = useHistory()
+
+    const colorClicker = (color)=>{
+        history.push(Route.profile, color)
+    }
+
     return (
         <div className='sidebar'>
             <div className='sidebar_top'>
@@ -16,6 +24,27 @@ function LeftSidebar() {
                 </div>
                 <h2>{User.name}</h2>
                 <h4>{User.email}</h4>
+            </div>
+            <div className='sidebar_bot'>
+                <h1>Choose a color:</h1>
+                <div className='colorPalette'>
+                    <div className='colorContainer' >
+                        <div id='p1' onClick={()=>colorClicker('Red')}></div>
+                        <h2>Red</h2>
+                    </div>
+                    <div className='colorContainer' >
+                        <div id='p2' onClick={()=>colorClicker('Blue')}></div>
+                        <h2>Blue</h2>
+                    </div>
+                    <div className='colorContainer'>
+                        <div id='p3' onClick={()=>colorClicker('Green')}></div>
+                        <h2>Green</h2>
+                    </div>
+                    <div className='colorContainer'>
+                        <div id='p4' onClick={()=>colorClicker('Yellow')}></div>
+                        <h2>Yellow</h2>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/view/profile/ProfilePage.js
+++ src/view/profile/ProfilePage.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ProfileSideImg from '../../shared/img/profileSideImg.jpg'
 import './ProfilePage.css'
 import Route from '../../routes/RoutingPath'
-import {useHistory} from 'react-router-dom'
+import {useHistory, useLocation} from 'react-router-dom'
 
 function ProfilePage() {
+    const location = useLocation()
     const history = useHistory()
 
     const returnHome=(()=>{
@@ -21,6 +22,12 @@ function ProfilePage() {
                 className='homeBtn'
                 onClick={returnHome}
                 >Home</button>
+                <div className='colorPaletteLanding'>
+                    <h2>Is this your color?</h2>
+                    <div className='chosenColor'>
+                        <h1>{location.state}</h1>
+                        </div>
+                </div>
             </div>
             <div className='rightSide'>
                 <img src={ProfileSideImg} alt="" />
